dfc6de7f2ef8b826010b42fb0c45d34c25ae3e94
Koval Yaroslav <koval@q-writer.com>
2022-01-22 18:24:32 +0200

    Update core.js

--- core.js
+++ core.js
@@ -28,10 +28,13 @@ window.addEventListener("load", function () {
       if ([
         "created_at_utc"
       ].indexOf(keys_[i]) > -1) {
-        json_body["data"][keys_[i]] = get_time(new Date(json_body["data"][keys_[i]]))
+        json_body["data"][keys_[i]] = get_time(
+          new Date(json_body["data"][keys_[i]]))
       }
-      let name_ = `<div class="col themed-grid-col">${json_body["hint"][keys_[i]]}</div>`
-      let varb_ = `<div class="col themed-grid-col">${json_body["data"][keys_[i]]}</div>`
+      let name_ = `<div class="col themed-grid-col">
+        ${json_body["hint"][keys_[i]]}</div>`
+      let varb_ = `<div class="col themed-grid-col">
+        ${json_body["data"][keys_[i]]}</div>`
       array_ = array_+name_+varb_
     }
     return array_
@@ -41,7 +44,9 @@ window.addEventListener("load", function () {
     const lines_ = json_body.trades
     let array_ = "", status_ = ""
     for (var i = 0; i < lines_.length; i += 1) {
-      lines_[i]["trade_time"] = get_time(new Date(lines_[i]["trade_time"]))
+      lines_[i]["trade_time"] = get_time(
+        new Date(lines_[i]["trade_time"])
+      )
       if (!lines_[i]["profit"] && !lines_[i]["profit_uah"]) {
         lines_[i]["profit"] = "-"
         lines_[i]["profit_uah"] = "-"
@@ -49,9 +54,12 @@ window.addEventListener("load", function () {
       } else {
         status_ = `<div class="col themed-grid-col">Продаж</div>`
       }
-      let time_ = `<div class="col themed-grid-col">${lines_[i]["trade_time"]}</div>`
-      let profit_ = `<div class="col themed-grid-col">${lines_[i]["profit"]}</div>`
-      let profituah_ = `<div class="col themed-grid-col">${lines_[i]["profit_uah"]}</div>`
+      let time_ = `<div class="col themed-grid-col">
+        ${lines_[i]["trade_time"]}</div>`
+      let profit_ = `<div class="col themed-grid-col">
+        ${lines_[i]["profit"]}</div>`
+      let profituah_ = `<div class="col themed-grid-col">
+        ${lines_[i]["profit_uah"]}</div>`
       array_ = array_+time_+profit_+profituah_+status_
     }
     return array_
