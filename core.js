24a541bad753c8c11806960ff9813bdd744a7fa3
Koval Yaroslav <koval@q-writer.com>
2022-01-23 20:01:05 +0200

    Update core.js

--- core.js
+++ core.js
@@ -7,23 +7,29 @@ window.addEventListener("load", function () {
     })
   }
   
+  function line_builder(data, following = true) {
+    var result = ""
+    for (var i = 0; i < data.length; i += 1) {
+      let first_el_modify = ""
+      if (!following) { first_el_modify = "style=\"border-top:0\"" }
+      result+`<div class="col themed-grid-col" ${first_el_modify}>${data[i]}</div>`
+    }
+    return result
+  }
+  
   function build_table(json_body) {
     const keys_ = Object.keys(json_body.data)
-    let array_ = ""
+    var array_ = ""
     for (var i = 0; i < keys_.length; i += 1) {
-      let first_el_modify = ""
-      if (i == 0) { first_el_modify = "style=\"border-top:0\"" }
       if ([
         "created_at_utc"
       ].indexOf(keys_[i]) > -1) {
         json_body["data"][keys_[i]] = get_time(
           new Date(json_body["data"][keys_[i]]))
       }
-      const name_ = `<div class="col themed-grid-col" ${first_el_modify}>
-        ${json_body["hint"][keys_[i]]}</div>`
-      const varb_ = `<div class="col themed-grid-col" ${first_el_modify}>
-        ${json_body["data"][keys_[i]]}</div>`
-      array_ = array_+name_+varb_
+      array_ = array_+line_builder([
+        json_body["hint"][keys_[i]], json_body["data"][keys_[i]]
+      ], keys_[i])
     }
     return array_
   }
@@ -38,17 +44,12 @@ window.addEventListener("load", function () {
       if (!lines_[i]["profit"] && !lines_[i]["profit_uah"]) {
         lines_[i]["profit"] = "-"
         lines_[i]["profit_uah"] = "-"
-        status_ = `<div class="col themed-grid-col">Купівля</div>`
-      } else {
-        status_ = `<div class="col themed-grid-col">Продаж</div>`
-      }
-      let time_ = `<div class="col themed-grid-col">
-        ${lines_[i]["trade_time"]}</div>`
-      let profit_ = `<div class="col themed-grid-col">
-        ${lines_[i]["profit"]}</div>`
-      let profituah_ = `<div class="col themed-grid-col">
-        ${lines_[i]["profit_uah"]}</div>`
-      array_ = array_+time_+profit_+profituah_+status_
+        status_ = "Купівля"
+      } else { status_ = "Продаж" }
+      array_ = array_+line_builder([
+        lines_[i]["trade_time"], lines_[i]["profit"],
+        lines_[i]["profit_uah"], status_
+      ], lines_[i])
     }
     return array_
   }
