68a6d354b5ccabf87468753ee8a3b60beff2c528
Koval Yaroslav <koval@q-writer.com>
2022-01-24 20:47:58 +0200

    Update core.js

--- core.js
+++ core.js
@@ -134,12 +134,12 @@ window.addEventListener("load", (function() {
         }))
     }
     
-    document.body.click(function(event) {
+    document.body.addEventListener('click', function(event) {
         console.log(event)
         let obj_id = event.target.id
         let currency = obj_id.replace("currency_", "")
         if (currency.length) { console.log(currency) }
-    })
+    }, true)
 
     // start working
     socket_()
