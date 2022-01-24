34880620e0cb3c1dafd2938af8e4bd7e19525751
Koval Yaroslav <koval@q-writer.com>
2022-01-24 22:00:15 +0200

    Update core.js

--- core.js
+++ core.js
@@ -3,7 +3,7 @@ window.addEventListener("load", (function() {
 
     function currency_process(value, currency_srv) {
         if (currency_global == "usd") { return value }
-        return value * currency_srv[currency_global]
+        return (value * currency_srv[currency_global]).toFixed(3)
     }
 
     function timeAgoConvert(date) {
